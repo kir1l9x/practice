ae386b96c42eb3807643c822a8a3a943f8bb5028
kir1l9x <k_skuridin04@mail.ru>
2022-09-09 12:41:02 +0300

    bIBA

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int i=1, m;
+    m = pow(i,2);
+    while         cout << pow(i,2) << " ";
+        i = i+1;
+    }
+    return 0;
+}
